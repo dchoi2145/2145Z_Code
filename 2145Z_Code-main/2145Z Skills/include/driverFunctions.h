de779cdaf8af863836a27594852fbc9e57efb897
dchoi2145 <63204411+dchoi2145@users.noreply.github.com>
2020-12-11 00:09:02 -0500

    pp

---
+++ 2145Z_Code-main/2145Z Skills/include/driverFunctions.h
@@ -0,0 +1,11 @@
+#ifndef DRIVERFUNCTIONS_H
+#define DRIVERFUNCTIONS_H
+
+int chassisMovement();
+int Intake();
+void printLineValue1(void);
+void printLineValue2(void);
+void printOptical();
+void printInet();
+void opticalLight();
+#endif
---
+++ 2145Z_Code-main/include/autonFunctions.h
@@ -0,0 +1,36 @@
+#ifndef AUTONFUNTIONS_H
+#define AUTONFUNTIONS_H
+
+void inertialCalibration();
+void reset();
+void moveForward(int distance, int speed);
+void backward(int distance, int speed);
+void turnRight(int distance, int speed);
+void turnLeft(int distance, int speed);
+void intakeRollers(int distance, int speed);
+void outtakeRollers(int distance, int speed);
+void intakeConveyor(int distance, int speed);
+void outtakeConveyor(int distance, int speed);
+void forwardTime(int speed);
+void backwardTime(int speed);
+void forwardPID(int target);
+void backwardPID(int target);
+void rightPID(int target);
+void leftPID(int target);
+void forwardSlowPID(int target);
+void backwardSlowPID(int target);
+void rightSlowPID(int target);
+void leftSlowPID(int target);
+void opticalStopRed();
+void opticalStopBlue();
+void ballStopRed();
+void ballStopBlue(int speed);
+void ballStopAll();
+void rollerSpeed(int speed);
+void conveyorSpeed(int speed);
+void setCoast();
+void setHold();
+void flipOut(void);
+void allSpin(int speed);
+void printInet();
+#endif
